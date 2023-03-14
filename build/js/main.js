bb5af122c8012625d98ad0a3fe47f275005ed45a
khanglb2000 <lambaokhang2000@gmail.com>
2023-03-14 14:27:51 +0700

    Add mobile menu

---
+++ build/js/main.js
@@ -0,0 +1,14 @@
+function initApp() {
+  const menuBtn = document.getElementById('mobile-open-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('flex');
+  };
+
+  menuBtn.addEventListener('click', toggleMenu);
+  mobileMenu.addEventListener('click', toggleMenu);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
